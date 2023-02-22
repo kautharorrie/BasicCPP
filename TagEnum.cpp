542596be7237026712b05bf952f7ac6c1ac7506f
kautharorrie <orrkau001@myuct.ac.za>
2023-02-22 09:22:08 +0200

    intial files, contains one method, extractFileContent (no implementation)

---
+++ TagEnum.cpp
@@ -0,0 +1,13 @@
+
+#include "TagEnum.h"
+
+
+	
+void ORRKAU001::extractFileContents(std::string filename)
+{
+	std::cout << filename << std::endl; //only a print to screen test if method works, no functionality
+	std::vector<std::string> test_vector; 
+	
+}
+
+
---
+++ TagEnum.h
@@ -0,0 +1,27 @@
+/**
+ * .h file 
+ * Simple Tag Enumeration Assignment 1 
+ * CSC3022F 
+ * Author: Kauthar Orrie
+ *
+ */
+ 
+#ifndef TAGENUM
+#define TAGENUM
+
+//includes 
+#include "string.h"
+#include <iostream>
+#include <ctype.h>
+#include <stdio.h>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+namespace ORRKAU001
+{
+	void extractFileContents(std::string filename); 
+}
+
+
+#endif
